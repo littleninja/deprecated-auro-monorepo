2bf42680695cdb4078f0d75808cebcf1939e48c4
Dale Sande <dale.sande@alaskaair.com>
2020-06-29 10:59:26 -0700

    fix: update missed fallback CSS support

--- packages/auro-checkbox/scripts/postCss.js
+++ packages/auro-checkbox/scripts/postCss.js
@@ -5,7 +5,7 @@ const removeRules = require('postcss-remove-rules')
 const comments = require('postcss-discard-comments')
 const fs = require('fs')
 
-fs.readFile('src/style.css', (err, css) => {
+fs.readFile('src/auro-checkbox.css', (err, css) => {
   postcss([autoprefixer, postcssCustomProperties, comments])
     .use(comments({
       remove: function(comment) { return comment[0] == "@"; }
@@ -15,11 +15,30 @@ fs.readFile('src/style.css', (err, css) => {
         ':root': '*'
       }
     }))
-    .process(css, { from: 'src/style.css', to: 'src/style.css' })
+    .process(css, { from: 'src/auro-checkbox.css', to: 'src/auro-checkbox.css' })
     .then(result => {
-      fs.writeFile('src/style.css', result.css, () => true)
+      fs.writeFile('src/auro-checkbox.css', result.css, () => true)
       if ( result.map ) {
-        fs.writeFile('src/style.map', result.map, () => true)
+        fs.writeFile('src/auro-checkbox.map', result.map, () => true)
+      }
+    })
+})
+
+fs.readFile('src/auro-checkbox-group.css', (err, css) => {
+  postcss([autoprefixer, postcssCustomProperties, comments])
+    .use(comments({
+      remove: function(comment) { return comment[0] == "@"; }
+    }))
+    .use(removeRules({
+      rulesToRemove: {
+        ':root': '*'
+      }
+    }))
+    .process(css, { from: 'src/auro-checkbox-group.css', to: 'src/auro-checkbox-group.css' })
+    .then(result => {
+      fs.writeFile('src/auro-checkbox-group.css', result.css, () => true)
+      if ( result.map ) {
+        fs.writeFile('src/auro-checkbox-group.map', result.map, () => true)
       }
     })
 })
